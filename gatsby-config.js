4c6b2400b100f3aef849743a4fe5961340c5762b
oeyoews <jyao4783@gmail.com>
2023-04-04 12:26:55 +0800

    Automatic commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,13 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Starter Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `oeyoews`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `oeyoews`,
     },
   },
   plugins: [

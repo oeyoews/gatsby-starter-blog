01f42a185746a34841f706c360299fff70c6927c
oeyoews <jyao4783@gmail.com>
2023-04-06 18:30:59 +0800

    feat: change all posts to oeyoews

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,7 +71,7 @@ export default BlogIndex
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="All posts" />
+export const Head = () => <Seo title="oeyoews" />
 
 export const pageQuery = graphql`
   {

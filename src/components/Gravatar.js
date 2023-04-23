800ea4fc39ebd450bd230b2b0255f07689642a95
oeyoews <jyao4783@gmail.com>
2023-04-23 15:21:08 +0800

    perf: add props

--- src/components/Gravatar.js
+++ src/components/Gravatar.js
@@ -2,24 +2,23 @@ import * as React from "react"
 import useSound from "use-sound"
 import boopSfx from "./pop.mp3"
 
-// use timeout
-export default function Gravatar() {
+export default function Gravatar({ email = "2956398608", user = "oeyoews" }) {
   const [play] = useSound(boopSfx)
   return (
-    <span>
+    <div>
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href="https://github.com/oeyoews"
+        href={`https://github.com/${user}`}
         onClick={() => play()}
         role="button"
       >
         <img
-          src="https://q1.qlogo.cn/g?b=qq&nk=2956398608&s=100"
-          className="mx-2 h-4 rounded-full align-bottom shadow-lg transition duration-200 hover:scale-105"
+          src={`https://q1.qlogo.cn/g?b=qq&nk=${email}&s=100`}
+          className="mx-2 h-4 rounded-full align-bottom shadow-lg transition duration-200 hover:scale-125"
           alt="avatar"
         />
       </a>
-    </span>
+    </div>
   )
 }
